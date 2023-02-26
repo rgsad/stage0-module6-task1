1a56ca9f05340cb4c5c142524d261e40fddd72bc
rgsad <rsad3262@gmail.com>
2023-02-26 23:01:34 +0300

    start "Animal" task

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,24 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal (String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String answer = "";
+        String paws = this.numberOfPaws > 1 ? " paws " : " paw ";
+        if (hasFur) {
+            answer =  "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + paws + "and a fur.";
+        } else {
+            answer = "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + paws + "and no fur.";
+        }
+        return answer;
+    }
 }
